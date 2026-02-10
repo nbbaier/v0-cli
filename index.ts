86a1d02e755e4cf296da0d15369529d4f0a723fe
Nico Baier <nico.baier@gmail.com>
2026-02-10 12:45:45 -0600

    chore: add oc config files and remove index.ts

--- index.ts
+++
@@ -1,8 +0,0 @@
-import { v0 } from "v0-sdk";
-
-const chats = await v0.chats.find({});
-const projects = await v0.projects.getById({
-	projectId: "prj_PwnqYRWMkolzA3KDWKjHsTnDP3MF",
-});
-
-console.log(projects);
